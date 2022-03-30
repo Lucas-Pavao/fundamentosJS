08990da8f81308a304fcf06764838f7b9efff20d
Lucas <Lucaspavao89@gmail.com>
2022-03-30 14:44:51 -0300

    Novo fundamento

---
+++ questao10.js
@@ -0,0 +1,12 @@
+function divisivel(valor) {
+
+    if (valor % 3 == 0) {
+        return true
+    } else {
+        return false
+    }
+
+}
+
+console.log(divisivel(9))
+console.log(divisivel(4))
