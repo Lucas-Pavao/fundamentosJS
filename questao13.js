3060e297d13f8ab6959b792c869f7c953ec834bf
Lucas <Lucaspavao89@gmail.com>
2022-04-01 15:03:27 -0300

    Novo fundamento

---
+++ questao13.js
@@ -0,0 +1,35 @@
+function diaDaSemana(a) {
+
+    switch (a) {
+
+        case 1:
+            console.log("Domingo")
+            break
+        case 2:
+            console.log("Segunda")
+            break
+        case 3:
+            console.log("Terça")
+            break
+        case 4:
+            console.log("Quarta")
+            break
+        case 5:
+            console.log("Quinta")
+            break
+        case 6:
+            console.log("Sexta")
+            break
+        case 7:
+            console.log("Sabado")
+            break
+        default:
+            console.log("Dia " + a + " Nao existe")
+    }
+
+}
+
+
+diaDaSemana(1)
+diaDaSemana(5)
+diaDaSemana(9)
